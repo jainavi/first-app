9a99d1463e70c5d4fd4de01baf00fc8a9a886b00
Avi Jain <jainavi1408@gmail.com>
2023-05-05 14:26:45 +0530

    Learned - Adding Images - Adding Buttons

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:first_app/styled_text.dart';
-
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -9,6 +7,8 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
+
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -20,8 +20,22 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Hello Flutter!'),
+      child: Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Image.asset(
+            'assets/images/dice-2.png',
+            width: 200,
+          ),
+          TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  padding: const EdgeInsets.only(
+                    top: 20,
+                  ),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Rool Dice'))
+        ]),
       ),
     );
   }
